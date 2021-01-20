d5d8230358af87684b4aafacae0d6fc40c5c227b
Alexandru Popescu <alex@Alexandrus-iMac.local>
2021-01-20 17:30:29 +0100

    Initial commit

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register on emeraldDev's website</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+<table class="user-types">
+    <tbody>
+    <tr>
+        <th></th>
+        <td>Classic user</td>
+        <td>Pro user</td>
+    </tr>
+    <tr>
+        <td>Name color</td>
+        <td>Black</td>
+        <td style="color:#FFAA00;">Gold</td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
